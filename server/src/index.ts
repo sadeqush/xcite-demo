a41c294a54ff54b336239961f90196ce0090aeaa
Sadequs Haque <32276793+sadeqush@users.noreply.github.com>
2023-03-09 22:01:24 -0500

    Setup Prisma db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import app from "./app";
+import { prisma } from "./prisma";
 
 const APP_PORT = process.env.APP_PORT || 3000;
 
 app.listen(APP_PORT, async () => {
   console.log(`Listening on port ${APP_PORT}`);
+
+  try {
+    await prisma.$connect();
+    console.log("Successfully connected to Database.");
+  } catch {
+    console.error("Error connecting to Database.");
+    process.exit();
+  }
 });
