b46980c8dcc4c38d5f2d1902b2a309be2f89d3ee
Sadequs Haque <32276793+sadeqush@users.noreply.github.com>
2023-03-09 21:32:36 -0500

    Added Prisma to the project

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
 import cors from "cors";
 
-const APP_PORT = process.env.APP_PORT || 3000;
-
 // Initialization
 const app = express();
 
 // Configuration
-app.set("port", APP_PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
